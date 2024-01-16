5be8d0b8d4d6e84a2363c46ad19be8a72ad2cc30
szroly <sep.roland@gmail.com>
2024-01-16 19:29:21 +0100

    Completed implementation of vehicle routes and controllers

--- app/api/controllers/vehicles.js
+++ app/api/controllers/vehicles.js
@@ -2,7 +2,7 @@ const vehiclesModel = require('../models/vehicles')
 
 module.exports = {
     create: async (req,res) => {
-        console.log("req", req.body);
+        
         const { 
             manufacturer, 
             model, 
@@ -13,8 +13,7 @@ module.exports = {
             ccm,
             power
         } = req.body
-        console.log({manufacturer})
-        console.log({ccm});
+       
         try {
             const vehicle = await vehiclesModel.create({
                 manufacturer, 
@@ -30,5 +29,85 @@ module.exports = {
         } catch(e) {
             console.log(e);
         }
+    },
+
+    showVehicles: async (req, res) => {
+        try {
+            const vehicles = await vehiclesModel.find({})
+            res.status(200).send(vehicles)
+        } catch (e) {
+            console.log(e)
+            res.status(500).send("Internal Server Error")
+        }
+    },
+
+    showVehicle: async (req, res) => {
+        const { id: vehicleId } = req.params
+        try {
+            const vehicle = await vehiclesModel.findOne({ _id: vehicleId })
+            if (vehicle) {
+                res.status(200).send(vehicle)
+            } else {
+                res.status(404).send({ message: 'Vehicle not found!' })
+            }
+        } catch (e) {
+            console.log(e)
+            res.status(500).send("Internal Server Error")
+        }
+    },
+
+    update: async (req, res) => {
+        const {
+            manufacturer,
+            model,
+            production_year,
+            chassis_number,
+            registration_expires,
+            registration_number,
+            ccm,
+            power
+        } = req.body
+
+        try {
+            const { id: vehicleId } = req.params
+            const updatedVehicle = await vehiclesModel.findOneAndUpdate(
+                { _id: vehicleId },
+                {
+                    $set: {
+                        manufacturer,
+                        model,
+                        production_year,
+                        chassis_number,
+                        registration_expires,
+                        registration_number,
+                        ccm,
+                        power
+                    }
+                },
+                { new: true } // Return the modified document rather than the original
+            )
+    
+            res.status(200).send(updatedVehicle)
+        } catch (e) {
+            console.log(e);
+            res.status(500).send("Internal Server Error")
+        }
+    },
+
+    delete: async (req,res) => {
+        const { id: vehicleId } = req.params
+
+        try {
+            const deletedVehicle = await vehiclesModel.findOneAndDelete({ _id: vehicleId })
+            if (deletedVehicle) {
+                res.status(200).send({ message: 'vehicle deleted successfully!' })
+            } else {
+                res.stataus(404).send({ message: 'Vehicle not found!' })
+            }
+        } catch (e) {
+            console.log(e)
+            res.status(500).send("Internal Server Error")
+        }
     }
+
 }
--- routes/vehicles.js
+++ routes/vehicles.js
@@ -3,6 +3,10 @@ const router = express.Router()
 const vehiclesController = require('../app/api/controllers/vehicles')
 const authMiddleware = require('../middleware/auth')
 
+router.get('/', authMiddleware.authenticateToken, vehiclesController.showVehicles)
+router.get('/:id', authMiddleware.authenticateToken, vehiclesController.showVehicle)
 router.post('/create', authMiddleware.authenticateToken, vehiclesController.create)
+router.put('/:id', authMiddleware.authenticateToken, vehiclesController.update)
+router.delete('/:id', authMiddleware.authenticateToken, vehiclesController.delete)
 
 module.exports = router
